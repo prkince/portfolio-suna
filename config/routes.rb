78c0e5fe29aa5d76af5db8fc6087eada3678362c
Pierrick Delage <pierrick.delage@gmail.com>
2020-04-21 18:09:35 +0200

    creating portfolio webpage

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   #root to: 'pages#welcome'
-  get '/welcome', to: 'pages#welcome'
+  get '/portfolio', to: 'pages#portfolio'
   root to: 'pages#welcome'
 end
